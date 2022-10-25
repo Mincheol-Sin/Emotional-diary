dba35b17171ece1c03a75f0ca6653edaf5c6615a
Mincheol Sin <106597531+Mincheol-Sin@users.noreply.github.com>
2022-10-25 17:36:28 +0900

    Feat: React에서 배열 사용하기1 = 리스트 렌더링
    - 배열 데이터를 props로 전달
    - 리스트를 컴포넌트로 만들어서 데이터 전달

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "신민철",
+    content: "하이 2",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "박서연",
+    content: "하이 3",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "김지환",
+    content: "하이 4",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: "도형림",
+    content: "하이 5",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
